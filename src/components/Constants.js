02ef0320dee07432c15878b7cae92463dc152b04
Nicolas Hemenway <nhemenway@catalystsolves.com>
2021-01-12 12:40:57 -0500

    tweaked error handling, tweaked form layout in CreatePatient

---
+++ src/components/Constants.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+
+const emailRegex = RegExp(/^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i);
+
+const zipcodeRegex = 'turbo';
+
+export { emailRegex, zipcodeRegex };
--- src/components/createPatient/CreatePatient.js
+++ src/components/createPatient/CreatePatient.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import loadImg from '../ajax-loader.gif';
 import styles from './CreatePatient.module.css';
+import { emailRegex, zipcodeRegex } from '../Constants';
 
 const axios = require('axios').default;
 
@@ -37,6 +40,22 @@ const CreatePatient = () => {
   const [weightError, setWeightError] = useState('');
   const [insuranceError, setInsuranceError] = useState('');
   const [genderError, setGenderError] = useState('');
+
+  const clearErrors = () => {
+    setFirstNameError('');
+    setLastNameError('');
+    setSsnError('');
+    setEmailError('');
+    setStreetError('');
+    setCityError('');
+    setStateError('');
+    setZipCodeError('');
+    setAgeError('');
+    setHeightError('');
+    setWeightError('');
+    setInsuranceError('');
+    setGenderError('');
+  };
 
   const handleChange = (event) => {
     switch (event.target.name) {
@@ -86,6 +105,18 @@ const CreatePatient = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    clearErrors();
+    let noValidate = false;
+
+    if (firstName === '' || !firstName.trim().length) {
+      setFirstNameError('Must be valid first name');
+      noValidate = true;
+    }
+
+    if (noValidate) {
+      // eslint-disable-next-line no-useless-return
+      return;
+    }
   };
 
   return (
@@ -98,19 +129,74 @@ const CreatePatient = () => {
       </h3>
       <>
         <form className={styles.form} onSubmit={handleSubmit} noValidate>
-          <div className={styles.input}><input type="text" name="firstName" placeholder="first name" onChange={handleChange} /></div>
-          <div className={styles.input}><input type="text" name="lastName" placeholder="last name" onChange={handleChange} /></div>
-          <div className={styles.input}><input type="text" name="ssn" placeholder="social security number" onChange={handleChange} /></div>
-          <div className={styles.input}><input type="email" name="email" placeholder="email" onChange={handleChange} /></div>
-          <div className={styles.input}><input type="text" name="street" placeholder="street" onChange={handleChange} /></div>
-          <div className={styles.input}><input type="text" name="city" placeholder="city" onChange={handleChange} /></div>
-          <div className={styles.input}><input type="text" name="state" placeholder="state" onChange={handleChange} /></div>
-          <div className={styles.input}><input type="text" name="zipcode" placeholder="zipcode" onChange={handleChange} /></div>
-          <div className={styles.input}><input type="number" name="age" placeholder="age" onChange={handleChange} /></div>
-          <div className={styles.input}><input type="number" name="height" placeholder="height" onChange={handleChange} /></div>
-          <div className={styles.input}><input type="number" name="weight" placeholder="weight" onChange={handleChange} /></div>
-          <div className={styles.input}><input type="text" name="insurance" placeholder="insurance" onChange={handleChange} /></div>
+          <div className={styles.input}>
+            <input type="text" name="firstName" placeholder="first name" onChange={handleChange} />
+            {' '}
+            <input type="text" name="lastName" placeholder="last name" onChange={handleChange} />
+          </div>
+          <div className={styles.inputError}>
+            <span className={styles.inputErrorPadding}>{firstNameError}</span>
+            {' '}
+            <span className={styles.inputErrorPadding}>{lastNameError}</span>
+          </div>
+
+          <div className={styles.input}>
+            <input type="text" name="ssn" placeholder="social security number" onChange={handleChange} />
+            {' '}
+            <input type="email" name="email" placeholder="email" onChange={handleChange} />
+          </div>
+          <div className={styles.inputError}>
+            <span className={styles.inputErrorPadding}>{ssnError}</span>
+            {' '}
+            <span className={styles.inputErrorPadding}>{emailError}</span>
+          </div>
+          
+          <div className={styles.input}>
+            <input type="text" name="street" placeholder="street" onChange={handleChange} />
+            {' '}
+            <input type="text" name="city" placeholder="city" onChange={handleChange} />
+          </div>
+          <div className={styles.inputError}>
+            <span className={styles.inputErrorPadding}>{streetError}</span>
+            {' '}
+            <span className={styles.inputErrorPadding}>{cityError}</span>
+          </div>
+
+          <div className={styles.input}>
+            <input type="text" name="state" placeholder="state" onChange={handleChange} />
+            {' '}
+            <input type="text" name="zipcode" placeholder="zipcode" onChange={handleChange} />
+          </div>
+          <div className={styles.inputError}>
+            <span className={styles.inputErrorPadding}>{stateError}</span>
+            {' '}
+            <span className={styles.inputErrorPadding}>{zipcodeError}</span>
+          </div>
+
+          <div className={styles.input}>
+            <input type="number" name="age" placeholder="age" onChange={handleChange} />
+            {' '}
+            <input type="number" name="height" placeholder="height" onChange={handleChange} />
+          </div>
+          <div className={styles.inputError}>
+            <span className={styles.inputErrorPadding}>{ageError}</span>
+            {' '}
+            <span className={styles.inputErrorPadding}>{heightError}</span>
+          </div>
+
+          <div className={styles.input}>
+            <input type="number" name="weight" placeholder="weight" onChange={handleChange} />
+            {' '}
+            <input type="text" name="insurance" placeholder="insurance" onChange={handleChange} />
+          </div>
+          <div className={styles.inputError}>
+            <span className={styles.inputErrorPadding}>{weightError}</span>
+            {' '}
+            <span className={styles.inputErrorPadding}>{insuranceError}</span>
+          </div>
           <div className={styles.input}><input type="text" name="gender" placeholder="gender" onChange={handleChange} /></div>
+          <div className={styles.inputError}>{genderError}</div>
+          <div><button type="submit">Create</button></div>
         </form>
       </>
     </div>
