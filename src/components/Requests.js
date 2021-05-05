cdb345c3379f2867337332ab99b865ecc1c91254
Nicolas Hemenway <nhemenway@catalystsolves.com>
2021-05-04 20:59:15 -0400

    got overall testing to >90%

--- src/components/Requests.js
+++ src/components/Requests.js
@@ -1,16 +1,16 @@
-const axios = require('axios').default;
+// const axios = require('axios').default;
 
-/**
- * Axios get request
- *
- * @param {string} endpoint is the endpoint to which the get request is made
- */
-const get = (endpoint) => axios.get(endpoint, {
-  headers: {
-    'Content-Type': 'application/json'
-    // mode: 'cors'
-  }
-});
+// /**
+//  * Axios get request
+//  *
+//  * @param {string} endpoint is the endpoint to which the get request is made
+//  */
+// const get = (endpoint) => axios.get(endpoint, {
+//   headers: {
+//     'Content-Type': 'application/json'
+//     // mode: 'cors'
+//   }
+// });
 
-// eslint-disable-next-line import/prefer-default-export
-export { get };
+// // eslint-disable-next-line import/prefer-default-export
+// export { get };
--- src/components/createEncounter/CreateEncounter.test.js
+++ src/components/createEncounter/CreateEncounter.test.js
@@ -40,7 +40,7 @@ const toRender = (id) => {
 
 const fillInputData = () => {
   userEvent.type(screen.getByTestId('notes'), 'test');
-  userEvent.type(screen.getByTestId('visit'), 'test');
+  userEvent.type(screen.getByTestId('visit'), 'A1S 3D1');
   userEvent.type(screen.getByTestId('provider'), 'test');
   userEvent.type(screen.getByTestId('billing'), '123.456.790-01');
   userEvent.type(screen.getByTestId('icd10'), 'B11');
@@ -50,7 +50,9 @@ const fillInputData = () => {
   userEvent.type(screen.getByTestId('pulse'), '1');
   userEvent.type(screen.getByTestId('systolic'), '1');
   userEvent.type(screen.getByTestId('diastolic'), '1');
-  userEvent.type(screen.getByTestId('date'), '20171015');
+  // userEvent.type(screen.getByTestId('date'), '10102015');
+  const input = screen.getByTestId('date');
+  fireEvent.change(input, { target: { value: '2015-10-10' } });
 };
 
 it('renders and matches snapshot', () => {
@@ -65,17 +67,70 @@ it('renders and matches snapshot', () => {
 
 it('updates encounter data', async () => {
   fetch
+    .mockResponseOnce('{ "id": 1 }', { status: 200, headers: { 'content-type': 'application/json' } })
     .mockResponseOnce({ status: 200, method: 'POST', headers: { 'content-type': 'application/json' } });
 
-  const { getByText, queryByText, getByTestId, getByAltText, debug } = render(toRender(1));
+  const { getByText, queryByText, getByTestId, getByAltText } = render(toRender(1));
 
   fillInputData();
-  debug();
 
+  await waitFor(async () => getByTestId('notes'));
   const selectButton = getByText('Create');
   fireEvent.click(selectButton);
   await waitFor(() => getByAltText('loading...'));
   expect(mockHistoryPush).toHaveBeenCalledWith('/patients/1');
+});
+
+it('checks if id is 404', async () => {
+  // need to change useParam, change from mock to spyOn
+  fetch
+    .mockResponse('{ "id": 1 }', { status: 404, headers: { 'content-type': 'application/json' } });
+
+  const { getByText, queryByText } = render(toRender('999'));
+
+  await waitFor(() => getByText('404 Patient Not Found'));
+  expect(screen.queryByText('404 Patient Not Found')).toBeTruthy();
+});
+
+it('checks if id is nan', async () => {
+  // need to change useParam, change from mock to spyOn
+  fetch
+    .mockResponse('{ "id": 1 }', { status: 400, headers: { 'content-type': 'application/json' } });
+
+  const { getByText, queryByText } = render(toRender('garbage'));
+
+  await waitFor(() => getByText('Patient id must be a number'));
+  expect(screen.queryByText('Patient id must be a number')).toBeTruthy();
+});
+
+it('catches generic error', async () => {
+  fetch
+    .mockReject(new Error('error'));
+
+  const { getByText, queryByText, getByTestId, getByAltText } = render(toRender(1));
+
+  fillInputData();
+
+  const selectButton = getByText('Create');
+  fireEvent.click(selectButton);
+  await waitFor(() => getByAltText('loading...'));
+  expect(queryByText('Oops something went wrong')).toBeTruthy();
+});
+
+it('tests if input error messages appear', async () => {
+  const { getByText, queryByText } = render(toRender(1));
+
+  const selectButton = getByText('Create');
+  fireEvent.click(selectButton);
+  await waitFor(() => getByText('Must be valid visit code'));
+  expect(queryByText('Must be valid visit code')).toBeTruthy();
+  expect(queryByText('Must be valid billing code')).toBeTruthy();
+  expect(queryByText('Must be valid provider')).toBeTruthy();
+  expect(queryByText('Must be valid total cost')).toBeTruthy();
+  expect(queryByText('Must be valid icd10')).toBeTruthy();
+  expect(queryByText('Must be valid chief complaint')).toBeTruthy();
+  expect(queryByText('Must be valid copay')).toBeTruthy();
+  expect(queryByText('Must be valid date')).toBeTruthy();
 });
 
 // test('component matches snapshot', () => {
